a182f29644a6e735f75a6760fd7abaa5383e6d45
Antoniosn4 <AntonioFilhof452@gmail.com>
2026-03-17 18:29:12 -0300

    feat(auth): implementa tela de login, JWT e middleware de protecao de rotas

---
+++ app/actions/auth.ts
@@ -0,0 +1,34 @@
+"use server";
+
+import prisma from "@/lib/prisma";
+import bcrypt from "bcryptjs";
+import { createSession } from "@/lib/session";
+import { redirect } from "next/navigation";
+
+export async function login(prevState: any, formData: FormData) {
+    const email = formData.get("email") as string;
+    const password = formData.get("password") as string;
+
+    if (!email || !password) {
+        return { error: "Email e senha são obrigatórios" };
+    }
+
+    // user from db
+    const user = await prisma.user.findUnique({
+        where: { email },
+    });
+
+    if (!user || !user.password) {
+        return { error: "Credenciais inválidas" };
+    }
+
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+
+    if (!isPasswordValid) {
+        return { error: "Credenciais inválidas" };
+    }
+
+    await createSession(String(user.id));
+
+    redirect("/dashboard");
+}
